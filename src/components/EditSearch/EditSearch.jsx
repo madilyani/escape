85324ff59707f8636e472444bc9e0ac3c8b12fc2
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2024-03-31 18:03:54 +0500

    asd

--- src/components/EditSearch/EditSearch.jsx
+++ src/components/EditSearch/EditSearch.jsx
@@ -8,6 +8,19 @@ export default function EditSearch() {
           <h5>modifica ricerca</h5>
           <div className="editSearch__close"></div>
         </div>
+        <div className="editSearch__content">
+          <div className="editSearch__row">
+            <div className="editSearch__date">
+              <h6>check in</h6>
+              <input type="text" placeholder="Seleziona Data" />
+            </div>
+            <hr />
+            <div className="editSearch__date">
+              <h6>check in</h6>
+              <input type="text" placeholder="Seleziona Data" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
