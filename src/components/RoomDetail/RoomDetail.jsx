1f9233e066be8b7f6299b6707aa2dc967498c453
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2024-03-31 14:57:15 +0500

    asd

--- src/components/RoomDetail/RoomDetail.jsx
+++ src/components/RoomDetail/RoomDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import RoomMobile from "components/RoomMobile";
-import { calendarIcon2, userIcon2 } from "Base/SVG";
+import { calendarIcon2, foodIcon, userIcon2 } from "Base/SVG";
 import CalendarMobile from "components/CalendarMobile";
 import moment from "moment";
 import Calendar from "components/Calendar";
@@ -87,7 +87,34 @@ export default function RoomDetail() {
                 <button type="button">Seleziona il viaggio</button>
               </div>
               <div className="roomItemInfo">
-                <div className="roomItemInfo__row"></div>
+                <div className="roomItemInfo__row">
+                  <div className="roomItemInfo__title">
+                    <h6>
+                      {foodIcon}
+                      Pensione Completa
+                    </h6>
+                    <div className="roomItemChecks">
+                      <div className="roomItemCheck">
+                        <div className="roomItemCheck__box">
+                          <input type="checkbox" />
+                          <span></span>
+                        </div>
+                        <p>
+                          Vista Mare <b>€ 70</b>{" "}
+                        </p>
+                      </div>
+                      <div className="roomItemCheck">
+                        <div className="roomItemCheck__box">
+                          <input type="checkbox" />
+                          <span></span>
+                        </div>
+                        <p>
+                        Culla<b>€ 25</b>{" "}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
